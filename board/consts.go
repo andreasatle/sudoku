99e41d3a9a97f9a9f14c147b731d3eb6757629cf
Andreas Atle <atle.andreas@gmail.com>
2020-06-06 22:21:08 -0500

    Added test for most rules.

--- board/consts.go
+++ board/consts.go
@@ -38,3 +38,61 @@ const nakedSinglesParam = `9 3 3
 -853-9-1-
 4----6--5
 `
+// Use same board for naked singles and hidden singles
+const hiddenSinglesParam = nakedSinglesParam
+
+const nakedPairsParam = `9 3 3
+ABCDEFGHI
+
+G--HDI-C-
+IBHACE--F
+D--BFG-HI
+FDBGHCIEA
+CIGDEAFBH
+HAEFIBC--
+B-DEAF-IC
+A----H-F-
+E----D-A-
+`
+// Replace this!
+const pointingPairsParam = `9 3 3
+123456789
+
+5--41--6-
+------23-
+-7-86---4
+9---8-7-6
+---------
+8-7-4---1
+7---56-4-
+-64------
+-5--74--9
+`
+
+const xWingsParam = `9 3 3
+123456789
+
+--38--51-
+--87--93-
+1--3-5728
+---2--849
+8-19-6257
+---5--163
+964127385
+382659471
+-1-4--692
+`
+
+const hiddenPairsParam = `9 3 3
+123456789
+
+-49132---
+-81479---
+327685914
+-96-518--
+-75-28---
+-38-46--5
+853267---
+712894563
+964513---
+`
--- board/rules_test.go
+++ board/rules_test.go
@@ -7,8 +7,11 @@ import (
 
 func TestRules(t *testing.T) {
 	TestNakedSingles(t)
-
-	t.Errorf("Add tests for remaining rules!")
+	TestHiddenSingles(t)
+	TestNakedPairs(t)
+	TestPointingPairs(t)
+	TestXWings(t)
+	TestHiddenPairs(t)
 }
 
 func TestNakedSingles(t *testing.T) {
@@ -20,7 +23,78 @@ func TestNakedSingles(t *testing.T) {
 
 	// Gauss-Seidel like update, gives more entries removed...
 	if finalBefore != 53 || finalAfter != 46 {
-		errMsg := fmt.Sprintf("NakedSingles fail (53,46): (%d,%d)", finalBefore, finalAfter)
+		errMsg := fmt.Sprintf("NakedSingles fail (Before:53,After:46): (%d,%d)", finalBefore, finalAfter)
+		t.Errorf(errMsg)
+	}
+}
+
+func TestHiddenSingles(t *testing.T) {
+	inputBoard := []byte(hiddenSinglesParam)
+	b := NewBoard(inputBoard)
+	finalBefore := b.finalValuesLeft
+	b.HiddenSingles()
+	finalAfter := b.finalValuesLeft
+
+	// Gauss-Seidel like update, gives more entries removed...
+	if finalBefore != 53 || finalAfter != 42 {
+		errMsg := fmt.Sprintf("HiddenSingles fail (Before:53,After:42): (%d,%d)", finalBefore, finalAfter)
+		t.Errorf(errMsg)
+	}
+}
+
+func TestNakedPairs(t *testing.T) {
+	inputBoard := []byte(nakedPairsParam)
+	b := NewBoard(inputBoard)
+	candidatesBefore := b.CountCandidates()
+	b.NakedPairs()
+	candidatesAfter := b.CountCandidates()
+
+	// Gauss-Seidel like update, gives more entries removed...
+	if candidatesBefore != 59 || candidatesAfter != 55 {
+		errMsg := fmt.Sprintf("NakedPairs fail (Before:57,After:55): (%d,%d)", candidatesBefore, candidatesAfter)
+		t.Errorf(errMsg)
+	}
+}
+
+func TestPointingPairs(t *testing.T) {
+	inputBoard := []byte(pointingPairsParam)
+	b := NewBoard(inputBoard)
+	candidatesBefore := b.CountCandidates()
+	b.PointingPairs()
+	candidatesAfter := b.CountCandidates()
+
+	// Gauss-Seidel like update, gives more entries removed...
+	if candidatesBefore != 198 || candidatesAfter != 180 {
+		errMsg := fmt.Sprintf("PointingPairs fail (Before:198,After:180): (%d,%d)", candidatesBefore, candidatesAfter)
+		t.Errorf(errMsg)
+	}
+}
+
+
+func TestXWings(t *testing.T) {
+	inputBoard := []byte(xWingsParam)
+	b := NewBoard(inputBoard)
+	candidatesBefore := b.CountCandidates()
+	b.XWings()
+	candidatesAfter := b.CountCandidates()
+
+	// Gauss-Seidel like update, gives more entries removed...
+	if candidatesBefore != 77 || candidatesAfter != 71 {
+		errMsg := fmt.Sprintf("XWings fail (Before:77,After:71): (%d,%d)", candidatesBefore, candidatesAfter)
+		t.Errorf(errMsg)
+	}
+}
+
+func TestHiddenPairs(t *testing.T) {
+	inputBoard := []byte(hiddenPairsParam)
+	b := NewBoard(inputBoard)
+	candidatesBefore := b.CountCandidates()
+	b.HiddenPairs()
+	candidatesAfter := b.CountCandidates()
+
+	// Gauss-Seidel like update, gives more entries removed...
+	if candidatesBefore != 68 || candidatesAfter != 67 {
+		errMsg := fmt.Sprintf("HiddenPairs fail (Before:68,After:67): (%d,%d)", candidatesBefore, candidatesAfter)
 		t.Errorf(errMsg)
 	}
 }
